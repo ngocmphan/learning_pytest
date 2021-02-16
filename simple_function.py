234be075edfe342c04507ab1d1a61e533fd7c9fe
ngocmphan <ngocm.phan@gmail.com>
2021-02-15 22:37:37 -0500

    Completed simple functions: addition func

--- simple_function.py
+++ simple_function.py
@@ -4,19 +4,29 @@ def prefix_average(S):
     A = [0]*n
     for j in range(n):
         total = 0
+        print(j)
         for i in range(j+1):
+            print(i)
             total += S[i]
-            print(total)
         A[j] = total / (j+1)
-        print(A[j])
     return A
 
 
-test = [1, 4]
-
-print(prefix_average(test))
+def prefix_average_2(s):
+    """Return list such that, for all j,
+    A[j]  equals average of s[0], ....s[j]"""
+    n = len(s)
+    A = [0]*n
+    for j in range(n):
+        A[j] = sum(s[0:j+1]) / (j+1)
+    return A
 
 
 def addition(s):
-
-    return
+    """Return a list such that, for all j,
+    A[j] equals the sum of previous values s[0:j]"""
+    n = len(s)
+    a = [0]*n
+    for j in range(n):
+        a[j] = sum(s[0:j+1])
+    return a
